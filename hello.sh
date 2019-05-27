bc7a92a4f8d6e405119115f7eeba5614b4b4850e
Official-David <maxdave92@gmail.com>
2019-05-27 12:43:55 +0100

    new update to bash script

--- hello.sh
+++ hello.sh
@@ -1,7 +1,10 @@
-#!/bin/bash
-num1=10
-num2=20
-sum $(($num1+$num2))
-echo $sum
 
-echo "now working"
+#!/bin/bash
+sudo mkdir automate
+cd automate
+sudo touch execute.sh
+cd ..
+sudo cp -r mydevtools.sh execute.sh
+git status
+git add .
+git commit -m "new update to bash script"
